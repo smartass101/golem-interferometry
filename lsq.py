36774dc2b76a5ec26c4f8ce675850dde81174576
Ondrej Grover <ondrej.grover@gmail.com>
2011-11-06 10:20:04 +0100

    added known parameters

--- lsq.py
+++ lsq.py
@@ -6,3 +6,11 @@
 from scipy import pi #used in sin() funcs
 from scipy.optimize import leastsq #the least square analyzer
 from numpy import loadtxt, sin #for loadidng the file and for sin()
+
+################ PARAMETERS ################
+
+f_base=5e5 #the modulation frequency, the base frequancy of the sine signal
+A_base=0.05 #the expected amplitude of the singal
+fname='ch1.csv'#file with the sine signal
+start_per=5 #how many periods to scan to obtain first approximation of parameters
+p0=[A_base, fbase, 0] #initial parameter sequnce to be passed to the leastsq
