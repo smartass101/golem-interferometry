afb255976be6b40dc9d67f7454dd6847ec1e386c
Ondrej Grover <ondrej.grover@gmail.com>
2011-11-06 10:14:09 +0100

    added importing of functions and constants

--- lsq.py
+++ lsq.py
@@ -1,5 +1,8 @@
 #!/usr/bin/python2
 """fit the sine signal with a sine function using the least squares method"""
 
+################ IMPORTING ################
 
-
+from scipy import pi #used in sin() funcs
+from scipy.optimize import leastsq #the least square analyzer
+from numpy import loadtxt, sin #for loadidng the file and for sin()
