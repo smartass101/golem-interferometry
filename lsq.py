da8552a0d80bc85acaac696e90fd4dfc060d21d8
Ondrej Grover <ondrej.grover@gmail.com>
2012-01-25 15:38:44 +0100

    actually use a fraction notation

--- lsq.py
+++ lsq.py
@@ -13,7 +13,7 @@ f_base=5e5 #the modulation frequency, the base frequancy of the sine signal
 A_base=0.05 #the expected amplitude of the singal
 fname='sin.csv'#file with the sine signal
 start_per=5 #how many periods to scan to obtain first approximation of parameters
-fit_per_frac= 4 #maximum time distance from root to points in fitted sample given by the fraction of the period
+fit_per_frac= 1./4 #maximum time distance from root to points in fitted sample given by the fraction of the period
 p0=[A_base, f_base, 0] #initial parameter sequnce to be passed to the leastsq
 
 ################ HELPER FUNCTIONS ################
@@ -45,7 +45,7 @@ def fit_sample(params0, start, length):
     """
     params, ok= leastsq(fitfunc, params0, args=(x[start:start + length], y[start:start + length]))
     if ok > 4 : #if the fitting didn't succeed
-        raise RuntimeError
+        raise RuntimeError("fit exited with error "+str(ok)+" at fit from point "+str(start)+" to "+str(length))
     else :
         params[2]=rephase(params[2]) #make sure it's the base phase
         if params[0] < 0 : #if the fitting resulted in negative amplitude
@@ -78,7 +78,7 @@ dt=x[1] - x[0] #calculate the time step
 period_len=round(1 / dt / f_base) #calculate the number of points in one period
 p0=fit_sample(p0, 0, period_len * start_per) #update the initial parametrs 
 roots=len(x) / period_len * 2 #calculate the expected number of roots along the way
-fit_distance=int(round(period_len / fit_per_frac)) #max distance of fitted points from root
+fit_distance=int(round(period_len * fit_per_frac)) #max distance of fitted points from root
 print "Initial parameters [amplitude, frequency, phase]: ",p0
 
 ################ DATA GENERATION ################
