d933aac44f892748f02cf13a73916b27157729b4
Ondrej Grover <ondrej.grover@gmail.com>
2012-03-08 12:18:25 +0100

    new concept: generate the roots sequence first, then work with it
    Rationalle: we need to iterate over the roots several times

--- roots.py
+++ roots.py
@@ -6,8 +6,7 @@
 ####                        IMPORTING                       ####
 ################################################################
 
-from scipy import pi #used in sin() funcs
-from numpy import loadtxt, nditer #for loadidng the file and for sin() and rounding, nditer() for looping over data
+from numpy import pi, loadtxt, savetxt, nditer, empty, array #for loadidng the file and for sin() and rounding, nditer() for looping over data
 ################################################################
 ####                        PARAMETERS                      ####
 ################################################################
@@ -15,7 +14,7 @@ from numpy import loadtxt, nditer #for loadidng the file and for sin() and round
 f_base=5e5 #the modulation frequency, the base frequency of the sine signal
 A_base=0.05 #the expected amplitude of the singal
 omega_base = f_base * 2 * pi #base angula frequency of the sine signal
-input_fname='sin.csv'#file with the sine signal
+input_fname='data/160833/ch1.csv'#file with the sine signal
 output_fname="phase-roots.csv"
 debug=True
 
@@ -30,8 +29,6 @@ else: #first run in session, must load data
     x, y=loadtxt(data_file, delimiter=',', unpack=True) #load and unpack the data
     print "Data loaded"
 
-output_file = open(output_fname, 'w')
-
 ################################################################
 ####                  INITIAL ANALYSIS                      ####
 ################################################################
@@ -40,29 +37,30 @@ dt=x[1] - x[0] #calculate the time step
 max_distance = 8 / f_base # minimum distance of roots, as a fraction of the period length
 
 ################################################################
-####                  DATA GENERATION                       ####
+####                  DATA EXTRACTION                       ####
 ################################################################
 
-roots = 0 #root count
-phase_integr = 0 #phase calculated by integrating phace changes
-t = 0 # occurrence of the current root
-t_last = 0 #storing occurrence of the last root
-iterator = nditer(y, flags=['c_index']) #generate an iterator object that will store the index in C order
-D_last_halfper = 0 #last root difference
-try: #will catch IndexError on last point
-    while not iterator.finished :
-        if  iterator[0] * y[iterator.index + 1] <= 0: # root found
-            if x[iterator.index + 1] - x[iterator.index] > max_distance: #roots too far
-                continue #TODO or could use this second "root" to calculate an even more precise root occurence
-            else: #seems to be a legit root
-                roots += 1
-                t = x[iterator.index +1] - y[iterator.index +1] * (x[iterator.index +1] - x[iterator.index]) / (y[iterator.index +1] - y[iterator.index]) #calculate the precise root in between through the secant method
-                if roots > 2:
-                    phase_integr += pi * (D_last_halfper - t + t_last) / D_last_halfper #phase change
-                    output_file.write("{:e},{:e}\n".format(t, phase_integr)) #write to output file as CVS format
-                D_last_halfper = t - t_last #store for future use
-                t_last = t #store for future use
-        iterator.iternext() #DO NOT forget this
-except IndexError:
-    pass #this is expected
-    
+roots = [] #root array
+iterator = nditer((x, y), flags=['c_index']) #generate an iterator object that will store the index in C order
+x_last = x[0]
+y_last = y[0]
+root_last = x[0]
+while not iterator.finished:
+    if iterator[1] * y_last <= 0 and iterator[0] - root_last > max_distance: # root found and is far enough
+        root_last = iterator[0] - iterator[1] * (iterator[0] - x_last) / (iterator[1] - y_last) #calculate the precise root in between through the secant method
+        roots.append(root_last)
+        x_last, y_last = iterator[0:2]
+    iterator.iternext()
+roots = array(roots)
+phase = empty(len(roots) - 1)
+
+################################################################
+####                 DATA GENERATION                        ####
+################################################################
+
+w_extra = phase[-1,1] / (phase[-1,0] - phase[0,0])
+
+for sample in phase:
+    sample[1] += sample[0] * w_extra
+
+savetxt(output_fname, phase)
